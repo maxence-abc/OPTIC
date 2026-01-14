7ecffbc1d5801f23efca94daa8af5d7da45bb7c3
maxence-abc <abric.maxence@etu.educ-valadon-limoges.fr>
2026-01-14 08:26:36 +0100

    update navbar and account css

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\AppointmentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,8 +13,22 @@ final class AccountController extends AbstractController
 {
     #[Route('/account', name: 'app_account')]
     #[IsGranted('ROLE_USER')]
-    public function index(): Response
+    public function index(AppointmentRepository $appointmentRepository): Response
     {
-        return $this->render('account/index.html.twig');
+        $user = $this->getUser();
+        if (!$user instanceof User) {
+            throw $this->createAccessDeniedException();
+        }
+
+        // Réservations réelles (côté client)
+        $appointments = $appointmentRepository->findBy(
+            ['client' => $user],
+            ['date' => 'DESC', 'startTime' => 'DESC']
+        );
+
+        return $this->render('account/index.html.twig', [
+            'appointments' => $appointments,
+        ]);
     }
 }
+    
